133fa8c8bd791afd1dde1e423f67bee479990f36
adam <meltymiko@gmail.com>
2024-04-04 13:34:25 -0400

    added gitignore and testconfig

---
+++ testconfig.py
@@ -0,0 +1,12 @@
+
+#INCLUDE FILE EXTENSIONS WHERE NEEDED
+
+# Location of file for URLs to be written to before download.
+listfile_location = "/absolute/link/to/list.txt"
+# Name of the exported bookmarks as a JSON file.
+# This should be saved in the current directory 
+bookmark_jsonfile = "bookmarks.json"
+# The name of the FOLDER within the bookmarks to be downloaded.
+bookmark_folder_name = "Folder"
+# The location where your videos will be downloaded to.
+download_location = "/absolute/folder/"
